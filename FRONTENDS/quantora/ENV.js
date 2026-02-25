9d605e26ea1629c9889404eef864c0a783bca8be
Bello Habeebullah <bellohabeebullah838@gmail.com>
2026-02-25 17:09:30 +0100

    .

---
+++ FRONTENDS/quantora/ENV.js
@@ -0,0 +1,9 @@
+
+const LOCAL_ENV = {
+    SERVICE_ID: import.meta.env.VITE_SERVICE_ID,
+    TEMPLATE_ID: import.meta.env.VITE_TEMPLATE_ID,
+    PUBLIC_KEY: import.meta.env.VITE_PUBLIC_KEY,
+    API_URL: import.meta.env.VITE_API_URL
+};
+
+export default LOCAL_ENV;
