01e81488baa5c038f710b918b77fc180bf1b84b7
Bello Habeebullah <bellohabeebullah838@gmail.com>
2026-02-26 18:23:48 +0100

    .

--- FRONTENDS/quantora/src/pages/Admin.jsx
+++ FRONTENDS/quantora/src/pages/Admin.jsx
@@ -23,7 +23,7 @@ export default function Admin() {
     setError("");
     setLoading(true);
     try {
-      const res = await fetch(`http://localhost:5000/api/auth/admin`, {
+      const res = await fetch(`https://quantora-4xrl.onrender.com/api/auth/admin`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
