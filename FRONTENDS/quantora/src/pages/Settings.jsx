3175d46c598adc0dea0d468017a13e06c14a0eb4
Bello Habeebullah <bellohabeebullah838@gmail.com>
2026-02-27 22:02:13 +0100

    .

--- FRONTENDS/quantora/src/pages/Settings.jsx
+++ FRONTENDS/quantora/src/pages/Settings.jsx
@@ -108,7 +108,7 @@ export default function Settings() {
                   <label className="text-[10px] uppercase font-black text-gray-500 tracking-widest block mb-2">Shop Name</label>
                   <input
                     name="shopName"
-                    value="Enter your shop name "
+                    value={formData.shopName}
                     onChange={handleChange}
                     className="w-full bg-white/[0.03] border border-white/10 rounded-xl px-4 py-3 outline-none focus:border-blue-500 transition-all text-white"
                   />
