8bc7f82d363df7e279900bb6f992368d1c66dbd8
Bello Habeebullah <bellohabeebullah838@gmail.com>
2026-03-01 11:11:54 +0100

    .

--- FRONTENDS/quantora/src/pages/Dashboard.jsx
+++ FRONTENDS/quantora/src/pages/Dashboard.jsx
@@ -159,7 +159,7 @@ export default function Dashboard() {
             {/* 1. ACTION CARDS */}
             <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
               <ActionCard 
-                title="Monthly Sales" 
+                title="Monthly Saless" 
                 icon="payments" 
                 desc={`₦${formatShortNumber(displayedSales)}`} 
                 isHighlight 
