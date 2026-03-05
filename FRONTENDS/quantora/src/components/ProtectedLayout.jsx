c8b080a156ea316cb045e4bb8ee87c7228dfd114
Bello Habeebullah <bellohabeebullah838@gmail.com>
2026-03-05 15:59:21 +0100

    .

--- FRONTENDS/quantora/src/components/ProtectedLayout.jsx
+++ FRONTENDS/quantora/src/components/ProtectedLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import SubscriptionGuard from "./SubscriptionGuard";
+import SubscriptionGuard from "./subscriptionGuard";
 
 export default function ProtectedLayout() {
   return (
